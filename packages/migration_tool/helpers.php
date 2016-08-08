2c1b1276f4f7a4cd089198dae0ed43e0f3a59551
Andrew Embler <andrew@concrete5.org>
2016-08-08 11:52:14 -0700

    helpers

--- packages/migration_tool/helpers.php
+++ packages/migration_tool/helpers.php
@@ -1,5 +1,7 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 
-function compat_is_version_8() {
-    return interface_exists('\Concrete\Core\Export\ExportableInterface');
+if (!function_exists('compat_is_version_8')) {
+    function compat_is_version_8() {
+        return interface_exists('\Concrete\Core\Export\ExportableInterface');
+    }
 }

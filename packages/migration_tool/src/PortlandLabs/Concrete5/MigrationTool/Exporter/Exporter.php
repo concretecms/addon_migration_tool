b61048a3c38739e19084ac1f85089d4cd83bf5e7
Andrew Embler <andrew@concrete5.org>
2017-12-14 11:21:14 -0800

    starting on including prefix in export xml

--- packages/migration_tool/src/PortlandLabs/Concrete5/MigrationTool/Exporter/Exporter.php
+++ packages/migration_tool/src/PortlandLabs/Concrete5/MigrationTool/Exporter/Exporter.php
@@ -63,10 +63,15 @@ class Exporter
         $files = array();
         $db = \Database::connection();
         foreach ($items as $item) {
-            $fID = $db->GetOne('select fID from FileVersions where fvFilename = ?', array($item));
+            if (strpos($item, ':') > -1) {
+                list($fvPrefix, $fvFilename) = explode(':', $item);
+                $fID = $db->GetOne('select fID from FileVersions where fvPrefix = ? and fvFilename = ?', array($fvPrefix, $fvFilename));
+            } else {
+                $fID = $db->GetOne('select fID from FileVersions where fvFilename = ?', array($item));
+            }
             if ($fID) {
                 $f = File::getByID($fID);
-                if (is_object($f) && !$f->isError()) {
+                if (is_object($f) && !$f->isError() && !in_array($f, $files)) {
                     $files[] = $f;
                 }
             }

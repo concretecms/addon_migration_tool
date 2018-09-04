60eafaf4409e44a8694261206dae9282d83f6952
Oliver Green <green2go@gmail.com>
2018-09-04 16:38:44 +0100

    Stop the importer erroring out while validating ImageSlider blocks in unknown areas.

--- packages/migration_tool/src/PortlandLabs/Concrete5/MigrationTool/Batch/Validator/Block/ImageSliderValidator.php
+++ packages/migration_tool/src/PortlandLabs/Concrete5/MigrationTool/Batch/Validator/Block/ImageSliderValidator.php
@@ -22,8 +22,13 @@ class ImageSliderValidator implements ItemValidatorInterface
         if ($record) {
             $data = $record->getData();
             if (isset($data['fsID']) && $data['fsID'] > 0) {
-                $page = $value->getBlock()->getArea()->getPage();
-                $messages->add(new Message(t('Slideshow block on page "%s" uses a file set for display. This will not migrate properly.', $page->getName())));
+                $pageName = 'Unknown Page';
+
+                if ($area = $value->getBlock()->getArea()) {
+                    $pageName = $area->getPage()->getName();
+                }
+                
+                $messages->add(new Message(t('Slideshow block on page "%s" uses a file set for display. This will not migrate properly.', $pageName)));
             }
         }
 
